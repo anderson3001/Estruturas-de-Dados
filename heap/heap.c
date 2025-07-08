c27b8fee18ccd02adbf04b9a167b620372d45c8e
Anderson Gomes <derson3001@gmail.com>
2025-07-07 22:20:15 -0300

    feat: heap em disco

--- heap/heap.c
+++ heap/heap.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define REG_TAM sizeof(Registro)
+
 typedef struct {
     char nome[51];
     char cpf[12];
@@ -9,114 +11,172 @@ typedef struct {
 } Registro;
 
 int pai(int i) {
-     return i / 2; 
-    }
+    return i / 2;
+}
 
 int esq(int i) {
-     return i * 2; 
-    }
+    return i * 2;
+}
 
 int dir(int i) {
-     return i * 2 + 1; 
-    }
-
-void subir(Registro* heap, int i) {
-    int j = pai(i);
-    if (j >= 1 && heap[i].nota > heap[j].nota) {
-        Registro temp = heap[i];
-        heap[i] = heap[j];
-        heap[j] = temp;
-        subir(heap, j);
-    }
+    return i * 2 + 1;
 }
 
-void descer(Registro* heap, int i, int n) {
-    int e = esq(i);
-    int d = dir(i);
-    int maior = i;
+Registro* leRegistro(FILE *arq) {
+    Registro *reg = (Registro*)malloc(sizeof(Registro));
+    if (!reg) return NULL;
 
-    if (e <= n && heap[e].nota > heap[maior].nota){
-        maior = e;
+    if (fread(reg, sizeof(Registro), 1, arq) != 1) {
+        free(reg);
+        return NULL;
     }
-    if (d <= n && heap[d].nota > heap[maior].nota){
-        maior = d;
-    }
-    if (maior != i) {
-        Registro temp = heap[i];
-        heap[i] = heap[maior];
-        heap[maior] = temp;
-        descer(heap, maior, n);
-    }
+    return reg;
 }
 
-Registro* inserir(Registro* heap, Registro novo, int* n) {
-    Registro* temp = realloc(heap, sizeof(Registro) * (*n + 2));
-    if (!temp) {
-        printf("Erro na alocação de memória.\n");
-        exit(1);
-    }
-    heap = temp;
-    (*n)++;
-    heap[*n] = novo;
-    subir(heap, *n);
-    return heap;
+void escreveReg(FILE *arq, Registro *reg) {
+    if (arq == NULL || reg == NULL) 
+    return;
+    fwrite(reg, sizeof(Registro), 1, arq);
 }
 
-void salvarHeap(Registro* heap, int n, const char* nomeArquivo) {
-    FILE* f = fopen(nomeArquivo, "wb");
-    if (!f) {
-        printf("Erro ao salvar heap");
+void subir(FILE* arq, int i) {
+    if (i <= 1) return;
+
+    int posPai = pai(i);
+    if (posPai < 1) return;
+
+    fseek(arq, (i - 1) * REG_TAM, SEEK_SET);
+    Registro *reg = leRegistro(arq);
+    if (!reg) return;
+
+    fseek(arq, (posPai - 1) * REG_TAM, SEEK_SET);
+    Registro *regPai = leRegistro(arq);
+    if (!regPai) {
+        free(reg);
         return;
     }
-    fwrite(&n, sizeof(int), 1, f);
-    fwrite(heap + 1, sizeof(Registro), n, f);
-    fclose(f);
+
+    if (reg->nota > regPai->nota) {
+        fseek(arq, (i - 1) * REG_TAM, SEEK_SET);
+        escreveReg(arq, regPai);
+
+        fseek(arq, (posPai - 1) * REG_TAM, SEEK_SET);
+        escreveReg(arq, reg);
+        fflush(arq);
+
+        free(reg);
+        free(regPai);
+        subir(arq, posPai);
+    } else {
+        free(reg);
+        free(regPai);
+    }
 }
 
-Registro* carregarRegistrosOriginais(int* total, const char* nomeArquivo) {
-    FILE* f = fopen(nomeArquivo, "rb");
-    if (!f) {
-        printf("Erro ao abrir registros.dat");
-        exit(1);
+void descer(FILE* arq, int i, int tam) {
+    Registro *reg = NULL, *regEsq = NULL, *regDir = NULL;
+    int e = esq(i);
+    int d = dir(i);
+    
+    fseek(arq, (i - 1) * REG_TAM, SEEK_SET);
+    reg = leRegistro(arq);
+    if (!reg) return;
+
+    if (e <= tam) {
+        fseek(arq, (e - 1) * REG_TAM, SEEK_SET);
+        regEsq = leRegistro(arq);
     }
 
-    fseek(f, 0, SEEK_END);
-    long tamanho = ftell(f);
-    rewind(f);
-
-    *total = tamanho / sizeof(Registro);
-    Registro* lista = malloc(tamanho);
-    fread(lista, sizeof(Registro), *total, f);
-    fclose(f);
-    return lista;
-}
-
-Registro* removerTopo(Registro* heap, int* n) {
-    if (*n == 0) {
-        printf("Heap já está vazio.\n");
-        return heap;
+    if (d <= tam) {
+        fseek(arq, (d - 1) * REG_TAM, SEEK_SET);
+        regDir = leRegistro(arq);
     }
 
-    printf("Removendo registro: %s (nota %.2f)\n", heap[1].nome, heap[1].nota);
-    heap[1] = heap[*n];
-    (*n)--;
-    heap = realloc(heap, sizeof(Registro) * (*n + 1));
-    descer(heap, 1, *n);
-    return heap;
+    int maior_pos = i;
+    Registro* regMaior = reg;
+
+    if (regEsq && regEsq->nota > regMaior->nota) {
+        maior_pos = e;
+        regMaior = regEsq;
+    }
+    if (regDir && regDir->nota > regMaior->nota) {
+        maior_pos = d;
+        regMaior = regDir;
+    }
+
+    if (maior_pos != i) {
+        fseek(arq, (i - 1) * REG_TAM, SEEK_SET);
+        escreveReg(arq, regMaior);
+
+        fseek(arq, (maior_pos - 1) * REG_TAM, SEEK_SET);
+        escreveReg(arq, reg);
+        fflush(arq);
+
+        descer(arq, maior_pos, tam);
+    }
+
+    free(reg);
+    if (regEsq) free(regEsq);
+    if (regDir) free(regDir);
 }
 
-void consultarTopo(Registro* heap, int n) {
-    if (n == 0) {
+int inserir(FILE* arq, Registro *novo, int tam) {
+    tam++;
+    fseek(arq, (tam - 1) * REG_TAM, SEEK_SET);
+    escreveReg(arq, novo);
+    fflush(arq);
+    subir(arq, tam);
+
+    return tam;
+}
+
+int removerTopo(FILE* arq, int tam) {
+    if (tam < 1) {
+        printf("Heap vazio.\n");
+        return tam;
+    }
+
+    fseek(arq, (tam- 1) * REG_TAM, SEEK_SET);
+    Registro *ultimo = leRegistro(arq);
+    if (!ultimo) {
+        return tam;
+    }
+
+    fseek(arq, 0, SEEK_SET);
+    escreveReg(arq, ultimo);
+    fflush(arq);
+
+    tam--;
+
+    free(ultimo);
+
+    descer(arq, 1, tam);
+
+    return tam;
+}
+
+void consultarTopo(FILE* arq, int tam) {
+    if (tam < 1) {
         printf("Heap vazio.\n");
         return;
     }
+
+    fseek(arq, 0, SEEK_SET);
+    Registro *reg = leRegistro(arq);
+    if (!reg) {
+        printf("Erro ao ler o topo.\n");
+        return;
+    }
+
     printf("\n[TOPO DO HEAP]\n");
-    printf("Nome: %s\n", heap[1].nome);
-    printf("CPF: %s\n", heap[1].cpf);
-    printf("Nota: %.2f\n", heap[1].nota);
+    printf("Nome: %s\n", reg->nome);
+    printf("CPF: %s\n", reg->cpf);
+    printf("Nota: %.2f\n", reg->nota);
+
+    free(reg);
 }
 
-void menuHeap(Registro* heap, int* n) {
+void menuHeap(FILE* arq, int tam) {
     int opcao;
     Registro novo;
 
@@ -132,7 +192,7 @@ void menuHeap(Registro* heap, int* n) {
 
         switch (opcao) {
             case 1:
-                consultarTopo(heap, *n);
+                consultarTopo(arq, tam);
                 break;
             case 2:
                 printf("Nome: ");
@@ -147,38 +207,101 @@ void menuHeap(Registro* heap, int* n) {
                 scanf("%f", &novo.nota);
                 getchar();
 
-                heap = inserir(heap, novo, n);
+                tam = inserir(arq, &novo, tam);
                 printf("Registro inserido com sucesso.\n");
                 break;
             case 3:
-                heap = removerTopo(heap, n);
+                tam = removerTopo(arq, tam);
                 break;
             case 4:
-                salvarHeap(heap, *n, "heap.dat");
-                printf("Heap salvo. Encerrando.\n");
-                break;
+                printf("Salvando e encerrando.\n");
+                fflush(arq);
+                return;
             default:
                 printf("Opção inválida.\n");
         }
 
-    } while (opcao != 4);
+    } while (1);
+}
+void imprimeHeap(FILE *arq, int tam){
+
+    if (arq == NULL) return;
+
+    Registro *reg = NULL;
+
+    printf("\n");
+    fseek(arq, 0, SEEK_SET);
+    
+    for (int i = 1; i <= tam; i++){
+        Registro *reg = leRegistro(arq);
+        if (reg == NULL) break;
+        printf("\nRegistro %d= { CPF= %s , NOME= %s, NOTA= %.2f}",i, reg->cpf, reg->nome, reg->nota);
+        free(reg);
+    }
+}
+
+int geraHeap(){
+    int tam = 0;
+    FILE *arq = fopen("./registros/registros.dat", "rb+");
+    FILE *arqHeap = fopen("./heap/heap.dat", "wb+");
+
+    if (arq == NULL) {
+        printf("ERRO: Nao foi possivel abrir './registros/registros.dat'. Verifique se o arquivo e a pasta existem.\n");
+        if (arqHeap) fclose(arqHeap);
+        return -1;
+    }
+    if (arqHeap == NULL) {
+        printf("ERRO: Nao foi possivel criar './heap/heap.dat'.\n");
+        fclose(arq);
+        return -1;
+    }
+    
+    for (int i = 0; i < 10000; i++){
+        Registro *reg = leRegistro(arq);
+
+        if (reg == NULL) {
+            break;
+        }
+
+        tam = inserir(arqHeap, reg, tam);
+        free(reg);
+    }
+    fclose(arq);
+    fclose(arqHeap);
+
+    return tam;
 }
 
 int main() {
-    int totalRegistros;
-    Registro* registros = carregarRegistrosOriginais(&totalRegistros, "./registros/registros.dat");
+    FILE* arq = fopen("./heap/heap.dat", "r+b"); 
 
-    Registro* heap = malloc(sizeof(Registro));
-    int nHeap = 0;
+    if (arq == NULL) {
+        printf("Arquivo heap.dat nao encontrado. Gerando uma nova heap...\n");
 
-    for (int i = 0; i < totalRegistros; i++) {
-        heap = inserir(heap, registros[i], &nHeap);
+        int registros = geraHeap(); 
+        if (registros < 0) {
+            printf("Ocorreu um erro durante a geracao da heap. Encerrando.\n");
+            return 1;
+        }
+
+        printf("Heap gerada com sucesso com %d registros.\n", registros);
+
+        arq = fopen("./heap/heap.dat", "r+b");
+        if (arq == NULL) {
+            printf("Erro critico: Nao foi possivel abrir a heap mesmo apos a geracao.\n");
+        }
+           
+    } else {
+        printf("Heap existente encontrada. Carregando...\n");
     }
+    fseek(arq, 0, SEEK_END);
+    long tam_bytes = ftell(arq);
+    int nHeap = (int)(tam_bytes / REG_TAM); 
 
-    free(registros);
+    printf("Heap aberta com %d elementos.\n", nHeap);
 
-    menuHeap(heap, &nHeap);
+    menuHeap(arq, nHeap);
 
-    free(heap);
+    fclose(arq);
     return 0;
 }
